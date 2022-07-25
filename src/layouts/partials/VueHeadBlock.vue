27835400ddadf61d493ae6a5bf897ff89063c4ec
nguyen dat <nguyenthanhdat22012001"gmail.com>
2022-07-25 16:41:05 +0700

    resolve conflic

--- src/layouts/partials/VueHeadBlock.vue
+++ src/layouts/partials/VueHeadBlock.vue
@@ -1,12 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <div class="w-100 bg-white px-6 py-5 flex justify-between shadow-sm relative">
-    <button><img src="@/assets/icons/bars.svg" alt="" /></button>
-    <div class="user flex gap-1 items-center pl-[22px] relative">
-      <img src="@/assets/icons/avatar-shop.png" />
-      <p class="text-dark-gray leading-4 font-semibold">BaobaoStore</p>
-      <vue-drop-down-user class="absolute"/>
-=======
   <div class="w-100 bg-white flex justify-between shadow-sm relative">
     <button><img src="@/assets/icons/bars.svg" alt="" class="px-6" /></button>
     <div
@@ -16,7 +8,6 @@
     >
       <img src="@/assets/icons/avatar-shop.png" />
       <p class="text-dark-gray leading-4 font-semibold">BaobaoStore</p>
->>>>>>> 066f821aa951bf822dbd90df07c67afa26957b41
     </div>
     <vue-drop-down-user
       v-show="isShowDropDown"
@@ -31,8 +22,6 @@ import VueDropDownUser from "./VueDropDownUser.vue";
 export default {
   components: {
     VueDropDownUser,
-<<<<<<< HEAD
-=======
   },
   data() {
     return {
@@ -54,7 +43,6 @@ export default {
         this.isShowDropDown = false;
       }
     },
->>>>>>> 066f821aa951bf822dbd90df07c67afa26957b41
   },
 };
 </script>
@@ -67,9 +55,6 @@ export default {
   height: 100%;
   transform: translateX(-22px);
 }
-<<<<<<< HEAD
-=======
 
 
->>>>>>> 066f821aa951bf822dbd90df07c67afa26957b41
 </style>
