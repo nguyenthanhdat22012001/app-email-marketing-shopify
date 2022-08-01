315af26a470d696be9e5776360212e98367a3578
nguyen dat <nguyenthanhdat22012001"gmail.com>
2022-08-01 11:48:50 +0700

    update heading extension custom tiptap

--- src/custom_extensions/heading/index.js
+++ src/custom_extensions/heading/index.js
@@ -7,37 +7,6 @@ import { mergeAttributes } from "@tiptap/core";
  */
 
 export default Heading.extend({
-  name: "heading",
-
-  addOptions() {
-    return {
-      levels: [1, 2, 3, 4, 5, 6],
-      HTMLAttributes: {},
-    };
-  },
-
-  content: "inline*",
-
-  group: "block",
-
-  defining: true,
-
-  addAttributes() {
-    return {
-      level: {
-        default: 1,
-        rendered: false,
-      },
-    };
-  },
-
-  parseHTML() {
-    return this.options.levels.map((level) => ({
-      tag: `h${level}`,
-      attrs: { level },
-    }));
-  },
-
   renderHTML({ node, HTMLAttributes }) {
     const hasLevel = this.options.levels.includes(node.attrs.level);
     const level = hasLevel ? node.attrs.level : this.options.levels[0];
@@ -74,28 +43,5 @@ export default Heading.extend({
       mergeAttributes(this.options.HTMLAttributes, HTMLAttributes),
       0,
     ];
-  },
-
-  addCommands() {
-    return {
-      setHeading:
-        (attributes) =>
-        ({ commands }) => {
-          if (!this.options.levels.includes(attributes.level)) {
-            return false;
-          }
-
-          return commands.setNode(this.name, attributes);
-        },
-      toggleHeading:
-        (attributes) =>
-        ({ commands }) => {
-          if (!this.options.levels.includes(attributes.level)) {
-            return false;
-          }
-
-          return commands.toggleNode(this.name, "paragraph", attributes);
-        },
-    };
   },
 });
