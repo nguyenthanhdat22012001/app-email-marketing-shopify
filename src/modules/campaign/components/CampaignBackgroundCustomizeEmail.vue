eb6db045cdddbceed2708428cb3993f470ec39f9
minhduc2312 <minhduc231298@gmail.com>
2022-08-01 13:04:15 +0700

    fix UI customize email

--- src/modules/campaign/components/CampaignBackgroundCustomizeEmail.vue
+++ src/modules/campaign/components/CampaignBackgroundCustomizeEmail.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="accordion-content bg-[#F2F2F5] pt-4 pb-5 px-5 rounded">
     <h5 class="font-bold text-dark mb-[15px]">Background</h5>
-    <div class="flex gap-4">
+
+    <div class="flex gap-5 mt-5">
       <div class="inline-flex flex-1 flex-col gap-[10px]">
         <span class="text-dark">Background color</span>
         <div class="bg-white flex items-center">
           <div
             class="flex flex-1 items-center py-[11px] px-[11px] border-r border-[#EBEBF0]"
           >
-            <input type="color" id="color" v-model="background.color" />
+            <input
+              type="color"
+              id="color"
+              v-model="background.color"
+            />
             <input
               class="text-xs text-black-light outline-none w-full uppercase"
               v-model="background.color"
@@ -19,22 +24,28 @@
               min="1"
               max="100"
               v-model="background.opacity"
+              type="number"
               class="w-[24px] text-center text-xs outline-none"
             />%
           </div>
         </div>
       </div>
       <div class="inline-flex flex-1 flex-col gap-[10px]">
-        <div
-          class="flex flex-1 items-center py-[11px] px-[11px] border-r border-[#EBEBF0]"
-        >
-          <input
-            type="number"
-            class="w-[100%] focus:outline-0 py-[1px]"
-            v-model="background.radius"
-          />
+        <span class="text-dark">Radius</span>
+        <div class="bg-white flex items-center flex-1">
+          <div
+            class="flex flex-1 items-center py-[11px] px-[11px] border-r border-[#EBEBF0]"
+          >
+            
+            <input
+              class="text-xs text-black-light outline-none w-full uppercase"
+              v-model="background.radius"
+            />
+          </div>
+          <div class="w-[50px] flex justify-center">
+            Px
+          </div>
         </div>
-        <span class="w-[40px] text-center text-xs">Px</span>
       </div>
     </div>
   </div>
