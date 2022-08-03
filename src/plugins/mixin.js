af88f3f83792b922185a144ec348193cbb2700b7
minhduc2312 <minhduc231298@gmail.com>
2022-08-03 17:55:51 +0700

    change name action

--- src/plugins/mixin.js
+++ src/plugins/mixin.js
@@ -1,12 +1,14 @@
+import notify from "@/helper/notify"
+
 export default {
     methods: {
         redirect(route) {
             this.$router.push(route)
         },
-        toastMessage() {
-
+        toastMessageError(title = "Error", message = "Something wrong!! Try again") {
+            notify.showNotify('error', title, message)
         },
-        getFullName(firstName,lastName){
+        getFullName(firstName, lastName) {
             return `${firstName} ${lastName}`
         }
     }
