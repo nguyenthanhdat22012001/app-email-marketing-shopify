e7b293345755be0e12a9fba3eaf4c5ac767f4498
nguyen dat <nguyenthanhdat22012001"gmail.com>
2022-08-17 22:09:57 +0700

    create modal confirm component, fux bug editor tiptap

--- src/modules/auth/api/index.js
+++ src/modules/auth/api/index.js
@@ -2,10 +2,10 @@ import { api } from '/src/plugins/api'
 
 export default {
     loginStore(payload) {
-        return api.post("https://865e-113-161-32-170.ap.ngrok.io/api/auth/login", payload);
+        return api.post("/api/auth/login", payload);
     },
 
     fetchUser() {
-        return api.get("https://865e-113-161-32-170.ap.ngrok.io/api/auth/getUser");
+        return api.get("/api/auth/getUser");
     },
 }
--- src/modules/campaign/router/index.js
+++ src/modules/campaign/router/index.js
@@ -19,7 +19,7 @@ const campaignRouter = [
                 name: 'campaign/create',
                 component: () => import(/* webpackChunkName: "campaign" */ "@/modules/campaign/views/CampaignCreate.vue"),
                 meta: {
-                    // middleware: [Auth]
+                    middleware: [Auth]
                 },
             }
         ]
