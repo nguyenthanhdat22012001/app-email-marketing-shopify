5a6767d0e54ff41ff262a13155ab97cc0ec19f22
nguyen dat <nguyenthanhdat22012001"gmail.com>
2022-08-25 18:00:06 +0700

    refactor

--- src/components/VSelectNumberRange.vue
+++ src/components/VSelectNumberRange.vue
@@ -24,16 +24,6 @@
             v-model="data_input.from"
             class="VSelectNumberRange__input w-[104px] px-2.5 py-2 border border-[#EBEBF0] rounded focus:outline-0"
           />
-          <div class="text-[12px] leading-5 mt-1 text-red" v-if="formstate">
-            <template v-if="!validation.form.input_from.required.valid">
-              <p>
-                {{ validation.form.input_from.required.message }}
-              </p></template
-            >
-            <template v-else-if="!validation.form.input_from.is_number.valid">
-              <p>{{ validation.form.input_from.is_number.message }}</p>
-            </template>
-          </div>
         </div>
         <div>
           <input
@@ -43,13 +33,7 @@
             class="VSelectNumberRange__input w-[104px] px-2.5 py-2 border border-[#EBEBF0] rounded focus:outline-0"
           />
           <div class="text-[12px] leading-5 mt-1 text-red" v-if="formstate">
-            <template v-if="!validation.form.input_to.required.valid">
-              <p>{{ validation.form.input_to.required.message }}</p></template
-            >
-            <template v-else-if="!validation.form.input_to.is_number.valid">
-              <p>{{ validation.form.input_to.is_number.message }}</p>
-            </template>
-            <template v-else-if="!validation.form.input_to.is_bigger.valid()">
+            <template v-if="!validation.form.input_to.is_bigger.valid()">
               <p>{{ validation.form.input_to.is_bigger.message() }}</p>
             </template>
           </div>
@@ -67,8 +51,6 @@
 
 <script>
 import VButton from "@/components/VButton.vue";
-
-import { validateIsNumber } from "@/helper/validates";
 
 export default {
   components: {
@@ -119,56 +101,50 @@ export default {
       };
       this.$emit("emitVSelectNumberRange", this.data_input);
     },
-    handleSetABSInput(from, to) {
-      this.data_input.from = Math.abs(from).toString();
-      this.data_input.to = Math.abs(to).toString();
+    handleSetABSNumber(number) {
+      if (number < 0 || number == "") {
+        return Math.abs(number).toString();
+      }
+      return number;
+    },
+    handleSetDataInput() {
+      if (this.data_input.from != "") {
+        this.data_input.from = this.handleSetABSNumber(this.data_input.from);
+      }
+      if (this.data_input.to != "" && this.data_input.from == "") {
+        this.data_input.from = this.handleSetABSNumber(this.data_input.from);
+        this.data_input.to = this.handleSetABSNumber(this.data_input.to);
+      }
+      if (this.data_input.to != "") {
+        this.data_input.to = this.handleSetABSNumber(this.data_input.to);
+      }
+      if (this.data_input.to != "" && this.data_input.from != "") {
+        this.data_input.from = this.handleSetABSNumber(this.data_input.from);
+        this.data_input.to = this.handleSetABSNumber(this.data_input.to);
+      }
     },
   },
   computed: {
     validation() {
-      if (this.data_input.from != "" || this.data_input.to != "") {
-        this.handleSetABSInput(this.data_input.from, this.data_input.to);
-      }
-      const input_from = {
-        required: {
-          valid: this.data_input.from ? true : false,
-          message: "This field is required!",
-        },
-        is_number: {
-          valid:
-            this.data_input.from && validateIsNumber(this.data_input.from)
-              ? true
-              : false,
-          message: "This field is number!",
-        },
-      };
+      this.handleSetDataInput();
+
+      const input_from = {};
 
       const input_to = {
-        required: {
-          valid: this.data_input.to ? true : false,
-          message: "This field is required!",
-        },
-        is_number: {
-          valid:
-            this.data_input.to && validateIsNumber(this.data_input.to)
-              ? true
-              : false,
-          message: "This field is number!",
-        },
         is_bigger: {
           valid: () => {
-            if (input_from.required.valid && input_to.is_number.valid) {
-              if (this.data_input.from > this.data_input.to) {
+            if (this.data_input.to && this.data_input.from) {
+              if (
+                parseInt(this.data_input.from) > parseInt(this.data_input.to)
+              ) {
                 return false;
               }
             }
             return true;
           },
           message: () => {
-            if (input_to.is_number.valid) {
-              if (this.data_input.from > this.data_input.to) {
-                return "This field is bigger!";
-              }
+            if (parseInt(this.data_input.from) > parseInt(this.data_input.to)) {
+              return "This field is bigger!";
             }
             return "";
           },
@@ -180,12 +156,7 @@ export default {
           input_from,
           input_to,
         },
-        valid:
-          input_from.required.valid &&
-          input_from.is_number.valid &&
-          input_to.required.valid &&
-          input_to.is_number.valid &&
-          input_to.is_bigger.valid(),
+        valid: input_to.is_bigger.valid(),
       };
     },
   },
