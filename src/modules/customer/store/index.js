237385ab00bc72d3c7f63f40ffcb7674bc8c5c24
nguyen dat <nguyenthanhdat22012001"gmail.com>
2022-08-26 16:11:28 +0700

    fefactor

--- src/modules/customer/store/index.js
+++ src/modules/customer/store/index.js
@@ -58,7 +58,6 @@ const actions = {
         return new Promise((resolve, reject) => {
             try {
                 const eventCustomersSync = (res) => {
-                    console.log(res);
                     if (state.progress < 100) {
                         commit('setProgress', Number(res.payload.processing));
                     }
